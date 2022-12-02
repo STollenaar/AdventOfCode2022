b17402ca36ec2e4a97b62396e244cba519e393ff
Stephen Tollenaar <stephen.tollenaar@radient360.com>
2022-12-02 13:56:43 -0330

    Done day 2

---
+++ day2/main.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Strat struct {
+	winMove  string
+	drawMove string
+	loseMove string
+}
+
+var (
+	stratMap1 map[string]Strat
+	stratMap2 map[string]Strat
+	valueMap  map[string]int
+
+	problem1Score int
+	problem2Score int
+)
+
+func init() {
+	stratMap1 = map[string]Strat{
+		"A": {
+			winMove:  "Y",
+			drawMove: "X",
+		},
+		"B": {
+			winMove:  "Z",
+			drawMove: "Y",
+		},
+		"C": {
+			winMove:  "X",
+			drawMove: "Z",
+		},
+	}
+
+	stratMap2 = map[string]Strat{
+		"A": {
+			winMove:  "B",
+			loseMove: "C",
+		},
+		"B": {
+			winMove:  "C",
+			loseMove: "A",
+		},
+		"C": {
+			winMove:  "A",
+			loseMove: "B",
+		},
+	}
+
+	valueMap = map[string]int{
+		"X": 1,
+		"Y": 2,
+		"Z": 3,
+		"A": 1,
+		"B": 2,
+		"C": 3,
+	}
+}
+
+func main() {
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		problem1(line)
+		problem2(line)
+	}
+
+	fmt.Printf("Solution problem1: %d\n", problem1Score)
+	fmt.Printf("Solution problem2: %d\n", problem2Score)
+}
+
+func problem1(line string) {
+	moves := strings.Split(line, " ")
+
+	strat := stratMap1[moves[0]]
+
+	problem1Score += valueMap[moves[1]]
+	if strat.winMove == moves[1] {
+		problem1Score += 6
+	} else if strat.drawMove == moves[1] {
+		problem1Score += 3
+	}
+}
+
+func problem2(line string) {
+	moves := strings.Split(line, " ")
+
+	strat := stratMap2[moves[0]]
+
+	switch moves[1] {
+	case "X":
+		problem2Score += valueMap[strat.loseMove]
+	case "Y":
+		problem2Score += valueMap[moves[0]]
+		problem2Score += 3
+	case "Z":
+		problem2Score += valueMap[strat.winMove]
+		problem2Score += 6
+	}
+}
